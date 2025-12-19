6874be3d6a16f41891b648866623998bf033a293
Farouk Talha <farouk.talha@gmail.com>
2025-12-19 16:11:55 +0100

    Fixing config-server tests: Explicitly added @ActiveProfiles("native") to test class

--- backend/config-server/src/test/java/com/ecommerce/configserver/ConfigServerApplicationTests.java
+++ backend/config-server/src/test/java/com/ecommerce/configserver/ConfigServerApplicationTests.java
@@ -3,7 +3,10 @@ package com.ecommerce.configserver;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.springframework.test.context.ActiveProfiles;
+
 @SpringBootTest
+@ActiveProfiles("native")
 class ConfigServerApplicationTests {
 
 	@Test
